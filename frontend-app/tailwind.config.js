4a19d67afb297ac4442a9c52f68f89d30ea2a63b
Bryan <bryanborck@gmail.com>
2023-08-22 00:35:54 -0300

    front initiated

---
+++ frontend-app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+      colors: {
+          primary: "#0BFFB7",
+          secondary: "#F5FFC9",
+          tertiary: "#D3D3D3",
+          black2: "#212121",
+          light: "#FCFCFC",
+          gray2: "#E0E0E0",
+      },
+      extend: {},
+  },
+  plugins: [],
+};
