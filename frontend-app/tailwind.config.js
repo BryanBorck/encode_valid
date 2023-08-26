ba6c2c0381c261524466dc4b8829701edd080614
Bryan <bryanborck@gmail.com>
2023-08-26 14:24:54 -0300

    colors

--- frontend-app/tailwind.config.js
+++ frontend-app/tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
       colors: {
-          primary: "#0BFFB7",
-          secondary: "#F5FFC9",
+          primary: "#4adf86",
+          secondary: "#3a3a3a",
           tertiary: "#D3D3D3",
           black2: "#212121",
           light: "#FCFCFC",
